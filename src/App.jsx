6cff750a17f1ff2a34d09c43c6ec539118696cd6
Heitor Gomes <heitorgomes900@gmail.com>
2023-01-18 01:07:31 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import { Header } from './components/Header';
+import { Post } from './components/Post';
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css';
+import './global.css';
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Heitor" content="loren ipsum ..." />
+          <Post author="Heitor" content="loren ipsum ..." />
+        </main>
+      </div>
+    </div>
+  )
+}
+
