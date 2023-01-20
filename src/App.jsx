d6c1902e095beb2007587a41b01236a21a273080
Heitor Gomes <heitorgomes900@gmail.com>
2023-01-19 22:24:43 -0300

    formatacao de datas

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,37 @@ import { Sidebar } from './components/Sidebar';
 import styles from './App.module.css';
 import './global.css';
 
+const posts = [
+  {
+    id: 1, 
+    author: {
+      avatarUrl: 'https://github.com/heitorgomes900.png',
+      name: 'Heitor Gomes',
+      role: 'Developer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋', },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀', },
+      { type: 'link', content: 'jane.design/doctorcare', },
+    ],
+    publishedAt: new Date('2023-01-03 20:00:00'),
+  },
+  {
+    id: 2, 
+    author: {
+      avatarUrl: 'https://github.com/heitorgomes900.png',
+      name: 'Heitor Gomes',
+      role: 'Developer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋', },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀', },
+      { type: 'link', content: 'jane.design/doctorcare', },
+    ],
+    publishedAt: new Date('2023-01-19 20:00:00'),
+  },
+];
+
 export function App() {
   const [count, setCount] = useState(0)
 
@@ -15,8 +46,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post author="Heitor" content="loren ipsum ..." />
-          <Post author="Heitor" content="loren ipsum ..." />
+          {posts.map(post => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />);
+          })}
+          
         </main>
       </div>
     </div>
